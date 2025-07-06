e37b2eb1c52fd1273eac054e11f3c960ee0aae97
Sumin <tnalsdldkr2@naver.com>
2025-07-06 09:25:26 +0900

    feat: ThemeProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider, createThemeConfig } from '@vapor-ui/core'
+import '@vapor-ui/core/styles.css'
+
+const themeConfig = createThemeConfig({
+  appearance: 'light',
+  radius: 'md',
+  scaling: 1.0,
+  storageKey: 'my-vite-app-theme', // index.html의 storageKey와 일치
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider config={themeConfig}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
